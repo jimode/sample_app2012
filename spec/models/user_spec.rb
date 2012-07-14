229650684de0581baaa891a07cce74bd42641dbc
Jimi Odeyemi <jimode68@gmail.com>
2012-07-14 19:25:55 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
   
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  
   it { should be_valid }
   
   describe "when name is not present" do
@@ -120,4 +124,9 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end 
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
